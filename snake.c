7d9c55dd1612f9c394d1349a7c854b42e3d766b1
Mogria <m0gr14@gmail.com>
2012-02-23 18:51:34 +0100

    implemented event handlers and fruits spawing on the game field

--- snake.c
+++ snake.c
@@ -8,6 +8,11 @@ typedef enum {
   DIR_LEFT = 3
 } direction;
 
+typedef struct fruits {
+  WINDOW** fruits;
+  int length;
+} FRUITS;
+
 typedef struct snake {
   WINDOW** parts;
   int length;
@@ -25,6 +30,49 @@ void kill_snake(SNAKE *snake);
 void grow_snake(SNAKE *snake, int posy, int posx);
 int move_snake(SNAKE *snake, int grow);
 WINDOW *snake_part_is_on(SNAKE *snake, int posy, int posx);
+void kill_fruits(FRUITS* fruits);
+void kill_fruit(FRUITS *fruits, int posy, int posx);
+WINDOW *fruit_is_on(FRUITS *fruits, int posy, int posx);
+
+void kill_fruits(FRUITS* fruits) {
+  int i;
+
+  for(i = 0; i < fruits->length; i++) {
+    delwin(fruits->fruits[i]);
+  }
+  fruits->length = 0;
+  free(fruits->fruits);
+}
+
+void kill_fruit(FRUITS *fruits, int posy, int posx) {
+  WINDOW *fruit;
+  int i;
+  if((fruit = fruit_is_on(fruits, posy, posx)) != NULL) {
+    for(i = 0; i < fruits->length; i++) {
+      if(fruits->fruits[i] == fruit) {
+        for(i++; i < fruits->length; i++) {
+          fruits->fruits[i-1] = fruits->fruits[i];
+        }
+        fruits->fruits = realloc(fruits->fruits, --fruits->length * sizeof(WINDOW*));
+        break;
+      }
+    }
+  }
+}
+  
+
+WINDOW *fruit_is_on(FRUITS *fruits, int posy, int posx) {
+  int i;
+  int y,x;
+
+  for(i = 0; i < fruits->length; i++) {
+    getbegyx(fruits->fruits[i], y, x);
+    if(posy == y && posx == x) {
+      return fruits->fruits[i];
+    }
+  }
+  return NULL;
+}
 
 void turn_left(direction *d) {
   *d  = (*d + 1) % MAX_DIR;
@@ -64,7 +112,44 @@ WINDOW *snake_part_is_on(SNAKE *snake, int posy, int posx) {
   return NULL;
 }
 
-#define EVENTS 1
+int grow_fruit(SNAKE* snake, FRUITS *fruits) {
+  int y,x,randy,randx;
+  getmaxyx(stdscr, y, x);
+  y--;
+  x--;
+  fprintf(logf, "beg x: %i, y: %i\n", x, y);
+  do {
+    randy = (rand() % y) + 1;
+    randx = (rand() % x) + 1;
+  } while (snake_part_is_on(snake, randy, randx) != NULL && fruit_is_on(fruits, randy, randx) != NULL);
+  fprintf(logf, "rand: %i\n", rand());
+  fprintf(logf, "x: %i, y: %i\n", randx, randy);
+
+  if(fruits->length == 0) {
+    fruits->fruits = malloc(sizeof(WINDOW*) * ++fruits->length);
+  } else {
+    fruits->fruits = realloc(fruits->fruits, sizeof(WINDOW*) * ++fruits->length);
+  }
+  
+  fruits->fruits[fruits->length - 1] = newwin(1, 1, randy, randx);
+  wprintw(fruits->fruits[fruits->length - 1], "%c", '.');
+  wrefresh(fruits->fruits[fruits->length - 1]);
+
+}
+
+#define EVENTS 2
+int check_border_collision(SNAKE *snake, int cury, int curx) {
+  int y,x;
+  getmaxyx(stdscr, y, x);
+  y--;
+  x--;
+  return cury <= 0 || curx <= 0 || cury >= y || curx >= x;
+}
+
+int check_border_collision_handler(SNAKE *snake) {
+  return 0;
+}
+
 int check_self_collision(SNAKE *snake, int cury, int curx) {
   WINDOW* on;
   return ! ((on = snake_part_is_on(snake, cury, curx)) == NULL || on == snake->parts[snake->length - 1]);
@@ -76,11 +161,13 @@ int check_self_collision_handler(SNAKE *snake) {
 
 int move_snake(SNAKE *snake, int grow) {
   int (*collision_checks[EVENTS])(SNAKE*,int,int) = {
+    check_border_collision,
     check_self_collision
   };
 
   int (*collision_handlers[EVENTS])(SNAKE*) = {
-    check_self_collision_handler
+    check_self_collision_handler,
+    check_border_collision_handler
   };
   int success = 1;
   int i;
@@ -114,7 +201,7 @@ int move_snake(SNAKE *snake, int grow) {
       grow_snake(snake, cury, curx);
     } else {
       WINDOW *cur = newwin(1, 1, cury, curx);
-      wprintw(cur, "%c", '.');
+      wprintw(cur, "%c", ' ');
       wrefresh(cur);
       delwin(cur);
     }
@@ -128,6 +215,9 @@ int main () {
   int columns;
   int success = 1;
   SNAKE snake = {};
+  FRUITS fruits = {};
+
+  srand(time(NULL));
 
   logf = fopen("game.log", "w");
   initscr();
@@ -160,6 +250,18 @@ int main () {
   grow_snake(&snake, rows / 2 - 4, columns / 2 - 9);
   grow_snake(&snake, rows / 2 - 5, columns / 2 - 9);
   grow_snake(&snake, rows / 2 - 6, columns / 2 - 9);
+
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
+  grow_fruit(&snake, &fruits);
   
   while((ch = getch()) && success) {
     if( ch == KEY_UP && snake.dir != DIR_DOWN) {
@@ -176,6 +278,8 @@ int main () {
   }
   getch();
   endwin();
+  kill_snake(&snake);
+  kill_fruits(&fruits);
   fclose(logf);
   return 0;
 }
