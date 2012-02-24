b6992beda4042f7af85ae243ba8606440f878a96
Mogria <m0gr14@gmail.com>
2012-02-24 19:08:46 +0100

    some bug fixing, the game works now basicly

--- snake.c
+++ snake.c
@@ -89,15 +89,17 @@ void kill_snake(SNAKE *snake) {
 }
 
 void grow_snake(SNAKE *snake, int posy, int posx) {
+  char schr = 'O';
   if(snake->length == 0) {
     snake->parts = malloc(sizeof(WINDOW*) * ++snake->length);
+    schr = '0';
   } else {
     snake->parts = realloc(snake->parts, sizeof(WINDOW*) * ++snake->length);
   }
   
 
   snake->parts[snake->length - 1] = newwin(1, 1, posy, posx);
-  wprintw(snake->parts[snake->length - 1], "%c", 'O');
+  wprintw(snake->parts[snake->length - 1], "%c", schr);
   wrefresh(snake->parts[snake->length - 1]);
 }
 
@@ -106,10 +108,13 @@ WINDOW *snake_part_is_on(SNAKE *snake, int posy, int posx) {
   int cury, curx;
   for(i = 0; i < snake->length; i++) {
     getbegyx(snake->parts[i], cury, curx);
+    fprintf(logf, "%i == %i && %i == %i\n", posy, cury, posx, curx);fflush(logf);
     if(cury == posy && curx == posx) {
+      fprintf(logf, "OK\n");fflush(logf);
       return snake->parts[i];
     }
   }
+  fprintf(logf, "nothing foun\n"); fflush(logf);
   return NULL;
 }
 
@@ -118,13 +123,10 @@ int grow_fruit(SNAKE* snake, FRUITS *fruits) {
   getmaxyx(stdscr, y, x);
   y--;
   x--;
-  fprintf(logf, "beg x: %i, y: %i\n", x, y);
   do {
     randy = (rand() % y) + 1;
     randx = (rand() % x) + 1;
   } while (snake_part_is_on(snake, randy, randx) != NULL && fruit_is_on(fruits, randy, randx) != NULL);
-  fprintf(logf, "rand: %i\n", rand());
-  fprintf(logf, "x: %i, y: %i\n", randx, randy);
 
   if(fruits->length == 0) {
     fruits->fruits = malloc(sizeof(WINDOW*) * ++fruits->length);
@@ -133,7 +135,7 @@ int grow_fruit(SNAKE* snake, FRUITS *fruits) {
   }
   
   fruits->fruits[fruits->length - 1] = newwin(1, 1, randy, randx);
-  wprintw(fruits->fruits[fruits->length - 1], "%c", '.');
+  wprintw(fruits->fruits[fruits->length - 1], "%c", '@');
   wrefresh(fruits->fruits[fruits->length - 1]);
 
 }
@@ -148,11 +150,12 @@ int check_fruit_collision_handler(SNAKE *snake, FRUITS *fruits, int cury, int cu
   kill_fruit(fruits, cury, curx);
   grow_fruit(snake, fruits);
   snake->grow++;
+  fprintf(logf, "fruit collision\n");fflush(logf);
   return 1;
 }
 
 int check_border_collision(SNAKE *snake, FRUITS *fruits, int cury, int curx) {
-  int y,x;
+  int y,x,back;
   getmaxyx(stdscr, y, x);
   y--;
   x--;
@@ -160,22 +163,28 @@ int check_border_collision(SNAKE *snake, FRUITS *fruits, int cury, int curx) {
 }
 
 int check_border_collision_handler(SNAKE *snake, FRUITS *fruits, int cury, int curx) {
+  fprintf(logf, "border collision %i %i\n", cury, curx);fflush(logf);
   return 0;
 }
 
 int check_self_collision(SNAKE *snake, FRUITS *fruits, int cury, int curx) {
+  int back = 0;
   WINDOW* on;
-  return ! ((on = snake_part_is_on(snake, cury, curx)) == NULL || on == snake->parts[snake->length - 1]);
+  fprintf(logf, "SNAKE PART ON %i %i\n", cury, curx);fflush(logf);
+  back = ! ((on = snake_part_is_on(snake, cury, curx)) == NULL || on == snake->parts[snake->length - 1]);
+  fprintf(logf, "returning %i\n", back);
+  return back;
 }
 
 int check_self_collision_handler(SNAKE *snake, FRUITS *fruits, int cury, int curx) {
+  fprintf(logf, "self collision\n");fflush(logf);
   return 0;
 }
 
 int move_snake(SNAKE *snake, FRUITS *fruits) {
   int (*collision_checks[EVENTS]) (SNAKE*, FRUITS*, int, int) = {
-    check_border_collision,
     check_self_collision,
+    check_border_collision,
     check_fruit_collision
   };
 
@@ -186,42 +195,59 @@ int move_snake(SNAKE *snake, FRUITS *fruits) {
   };
   int success = 1;
   int i;
+  int test = 0;
   int xdiff = snake->dir == DIR_LEFT ? -1 : (snake->dir == DIR_RIGHT ? 1 : 0);
   int ydiff = snake->dir == DIR_UP ? -1 : (snake->dir == DIR_DOWN ? 1 : 0);
+  fprintf(logf, "diff y: %i x: %i\n", ydiff, xdiff);fflush(logf);
   int curx;
-  int cury; 
-  int tmpy; 
+  int cury;
+  int tmpy;
   int tmpx;
-  getbegyx(snake->parts[snake->length -1], cury, curx);
+  getbegyx(snake->parts[0], cury, curx);
+  cury += ydiff;
+  curx += xdiff;
   for(i = 0; i < EVENTS && success; i++) {
-    if(collision_checks[i](snake, fruits, cury + ydiff, curx + xdiff)) {
-      if(!collision_handlers[i](snake, fruits, cury + ydiff, curx + xdiff)) {
+    if(test = collision_checks[i](snake, fruits, cury, curx)) {
+      fprintf(logf, "dafuq? %i\n", test);fflush(logf);
+      if(!collision_handlers[i](snake, fruits, cury, curx)) {
         success = 0;
       }
     }
   }
   if(success) {
-    mvwin(snake->parts[snake->length -1], cury + ydiff, curx + xdiff);
-    wrefresh(snake->parts[snake->length -1]);
-    for(i = snake->length - 2; i >= 0; i--) {
+    fprintf(logf, "y: %i, x: %i\n", cury, curx);fflush(logf);
+    mvwin(snake->parts[0], cury, curx);
+    fprintf(logf, "moved first window\n");fflush(logf);
+    wsyncup(snake->parts[0]);
+    wrefresh(snake->parts[0]);
+    fprintf(logf, "refreshed first window\n");fflush(logf);
+    for(i = 1; i < snake->length; i++) {
       getbegyx(snake->parts[i], tmpy, tmpx);
       mvwin(snake->parts[i], cury, curx);
+      fprintf(logf, "moved window\n");fflush(logf);
+      fprintf(logf, "x: %i, y: %i\n", cury, curx);fflush(logf);
       cury = tmpy;
       curx = tmpx;
       wsyncup(snake->parts[i]);
       wrefresh(snake->parts[i]);
+      fprintf(logf, "refreshed window\n");fflush(logf);
     }
 
+
+    fprintf(logf, "going to grow?\n");fflush(logf);
     if(snake->grow > 0) {
+      fprintf(logf, "yes\n");fflush(logf);
       grow_snake(snake, cury, curx);
       snake->grow--;
     } else {
+      fprintf(logf, "no\n");fflush(logf);
       WINDOW *cur = newwin(1, 1, cury, curx);
       wprintw(cur, "%c", ' ');
       wrefresh(cur);
       delwin(cur);
     }
   }
+  fprintf(logf, "back\n");fflush(logf);
   return success;
 }
 
@@ -251,21 +277,8 @@ int main () {
   refresh();
 
   grow_snake(&snake, rows / 2, columns / 2);
-  grow_snake(&snake, rows / 2 - 1, columns / 2);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 1);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 2);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 3);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 4);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 5);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 6);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 7);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 8);
-  grow_snake(&snake, rows / 2 - 1, columns / 2 - 9);
-  grow_snake(&snake, rows / 2 - 2, columns / 2 - 9);
-  grow_snake(&snake, rows / 2 - 3, columns / 2 - 9);
-  grow_snake(&snake, rows / 2 - 4, columns / 2 - 9);
-  grow_snake(&snake, rows / 2 - 5, columns / 2 - 9);
-  grow_snake(&snake, rows / 2 - 6, columns / 2 - 9);
+  grow_snake(&snake, rows / 2 + 1, columns / 2);
+  //snake.dir = DIR_LEFT;
 
   grow_fruit(&snake, &fruits);
   grow_fruit(&snake, &fruits);
