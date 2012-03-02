53b0d03d203c715803736dd0da9fb645df226f75
Mogria <m0gr14@gmail.com>
2012-03-02 19:28:33 +0100

    commented the run() function

--- snake.c
+++ snake.c
@@ -342,10 +342,9 @@ int display_menu() {
 }
 
 void run() {
-  int ch, ich;
-  int rows;
-  int columns;
-  int success = 1;
+  int ch, ich, rows, columns, success = 1;
+
+  // some variables for the timer (inclusive the interval)
   long nsec_before = 1000000;
   struct itimerspec delay;
   struct itimerspec get_delay;
@@ -357,14 +356,15 @@ void run() {
   struct sigevent event = {};
   event.sigev_notify = SIGEV_NONE;
 
+  // create the game struct
   GAME game = {};
-  game.highscore = 0;
 
-  int x,y;
+  // create a border
   border('#', '#', '#', '#', '#', '#', '#', '#');
   refresh();
 
-  getmaxyx(stdscr, rows, columns); // get the dimensions of the terminal
+  // get the dimensions of the terminal
+  getmaxyx(stdscr, rows, columns);
 
   // place the snake in the middle of the game field
   grow_snake(&game.snake, rows / 2, columns / 2);
@@ -372,23 +372,30 @@ void run() {
   glog("size %i", game.snake.length);
   game.snake.dir = DIR_LEFT;
 
+  // create some fruits on the screen
+  // NOM, NOM, NOM
   int i; 
   for(i = 0; i < 50; i++) {
     grow_fruit(&game);
   }
   
+  // get the time when the game started
   time(&game.started);
 
+  // create a timer
   int timerc = timer_create(CLOCK_REALTIME, &event, &timer);
   glog("timerc: %i", timerc);
   timer_settime(timer, 0, &delay, NULL);
-  
+
   while((ich = getch()) && success && ch != 'x') {
-    timer_gettime(timer, &get_delay);
+    // key typed?
     if(ich != ERR) {
       ch = ich;
     }
+    // check if we have an overrun
+    timer_gettime(timer, &get_delay);
     if((int)get_delay.it_value.tv_sec == 0 && (int)get_delay.it_value.tv_nsec > nsec_before) {
+      // new direction? 
       if((ch == KEY_UP || ch == 'w') && game.snake.dir != DIR_DOWN) {
         game.snake.dir = DIR_UP;
       } else if((ch == KEY_LEFT || ch == 'a') && game.snake.dir != DIR_RIGHT) {
@@ -398,15 +405,23 @@ void run() {
       } else if((ch == KEY_DOWN || ch == 's') && game.snake.dir != DIR_UP) {
         game.snake.dir = DIR_DOWN;
       }
+      // move the snake
       success = move_snake(&game);
+
+      // refresh the screen
       refresh();
-      timer_settime(timer, 0, &delay, NULL);
     }
     nsec_before = (long)get_delay.it_value.tv_nsec;
   }
+  // delete the timer
   timer_delete(timer);
+
+  // get the time when the game has ended
   time(&game.ended);
+
+  // display the highscore dialog
   display_highscore(&game);
+  // free all the resources reserved in the game struct
   kill_game(&game);
 }
 
