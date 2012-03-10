612f2cee97377ca8f0250628036325e1e9488596
Timo Furrer <info@ti-systems.ch>
2012-03-10 19:05:23 +0100

    Script to make debian package of msnake

---
+++ makedeb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+base=`readlink -mn $0`
+basedir=`dirname $base`
+basename=`basename $basedir`
+
+if [ ! -f "bin/msnake" ]; then
+  echo "msnake binary not found, thus let's compile!"
+  cmake .
+  make
+fi
+
+mkdir -p usr/bin/
+cp -f bin/msnake usr/bin
+
+cd $basedir/..
+
+dpkg-deb -z8 -Zgzip --build $basename
+cp -f msnake.deb $basedir
+
+echo "Created msnake.deb!"
