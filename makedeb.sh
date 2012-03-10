ceccddf69acbe5151d3f43fc7c4e28b3f9754f07
Timo Furrer <info@ti-systems.ch>
2012-03-10 19:31:46 +0100

    Refactored debian package building

--- makedeb.sh
+++ makedeb.sh
@@ -3,6 +3,11 @@
 base=`readlink -mn $0`
 basedir=`dirname $base`
 basename=`basename $basedir`
+debname=$basename.deb
+
+if [ ! -z $1 ]; then
+  debname=$1.deb
+fi
 
 if [ ! -f "bin/msnake" ]; then
   echo "msnake binary not found, thus let's compile!"
@@ -10,12 +15,13 @@ if [ ! -f "bin/msnake" ]; then
   make
 fi
 
-mkdir -p usr/bin/
-cp -f bin/msnake usr/bin
+mkdir -p package/usr/bin/
+cp -rf DEBIAN package
+cp -f bin/msnake package/usr/bin
 
-cd $basedir/..
+echo -n "Creating $debname ..."
+dpkg-deb -z8 -Zgzip --build package > /dev/null
+mv -f package.deb $basedir/$debname
+rm -rf package
 
-dpkg-deb -z8 -Zgzip --build $basename
-cp -f $basename.deb $basedir
-
-echo "Created msnake.deb!"
+echo " Done!"
