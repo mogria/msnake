47231ef8a5e4b1280d008fa855a5950e189ae481
Mogria <m0gr14@gmail.com>
2012-03-11 03:46:00 +0100

    the game only creates a `game.log` file if one is needed

--- src/glog.c
+++ src/glog.c
@@ -7,27 +7,29 @@ void glog(const char *format, ... ) {
   char *fformat;
   int length;
 
-  if(logfile == NULL) {
-    logfile = fopen(LOG_FILE, "w");
+  if(!(format == NULL && logfile == NULL)) {
     if(logfile == NULL) {
-      fprintf(stderr, "Cannot open logfile %s\n", LOG_FILE);
-      exit(EXIT_FAILURE);
+      logfile = fopen(LOG_FILE, "w");
+      if(logfile == NULL) {
+        fprintf(stderr, "Cannot open logfile %s\n", LOG_FILE);
+        exit(EXIT_FAILURE);
+      }
     }
-  }
 
-  if(format != NULL) {
-    length = strlen(format) + 2;
-    fformat = malloc(sizeof(char) * length);
-    strncpy(fformat, format, length - 2);
-    fformat[length - 2] = '\n';
-    fformat[length - 1] = '\0';
+    if(format != NULL) {
+      length = strlen(format) + 2;
+      fformat = malloc(sizeof(char) * length);
+      strncpy(fformat, format, length - 2);
+      fformat[length - 2] = '\n';
+      fformat[length - 1] = '\0';
 
-    va_start(args, format);
-    vfprintf(logfile, fformat, args);
-    fflush(logfile);
-    free(fformat);
-    va_end(args);
-  } else {
-    fclose(logfile);
+      va_start(args, format);
+      vfprintf(logfile, fformat, args);
+      fflush(logfile);
+      free(fformat);
+      va_end(args);
+    } else {
+      fclose(logfile);
+    }
   }
 }
