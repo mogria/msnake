c9a42f2cb882106d0a23ba9d4212fefb0dffdf11
Mogria <m0gr14@gmail.com>
2012-03-12 18:00:15 +0100

    added some comments to game.c

--- src/game.c
+++ src/game.c
@@ -5,22 +5,28 @@
 #include "main.h"
 #include "current_utc_time.h"
 
+// end the game, free the resources
 void kill_game(GAME *game) {
   kill_fruits(&game->fruits);
   kill_snake(&game->snake);
 }
 
 
+// calculate the difference between two timespec structs
 long long timeval_diff(struct timespec* tv1, struct timespec* tv2) {
   long long diff = 0;
+  // calculate the diffrence of the nanoseconds
   long nano = (tv2->tv_nsec - tv1->tv_nsec);
   int add = 0;
+
+  // some calculations if the nanoseconds of struct 1 were bigger
   if(nano < 0) {
     nano = 1000000000 + nano;
     add = 1;
   }
-  diff |= nano;
-  diff |= (tv2->tv_sec - tv1->tv_sec - add) * 2 * 32;
+  diff = nano;
+  // add the seconds
+  diff += (tv2->tv_sec - tv1->tv_sec - add) * 1000000000;
   return diff;
 }
 
@@ -101,11 +107,15 @@ void run() {
       last_time = current_time;
     }
 
+    // 'p' pressed
     if(ich == 'p') {
+      // show the pause dialog
       switch(pause_dialog()) {
         case 2:
+          // leave the game if '2' is pressed
           success = 0;
         default:
+          // redraw the screen on resume
           redraw_game(&game);
           break;
       }
@@ -157,9 +167,15 @@ int pause_dialog() {
 
 }
 
+// redraw the whole screen
 void redraw_game(GAME *game) {
+  // redraw the main window (containg the border and stuff)
   redrawwin(stdscr);
   refresh();
+  
+  // redraw the fruits
   redraw_fruits(&game->fruits);
+
+  // redraw the snake
   redraw_snake(&game->snake);
 }
